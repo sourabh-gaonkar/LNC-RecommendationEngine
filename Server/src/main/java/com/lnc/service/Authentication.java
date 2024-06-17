c4e64eda92709f5a91deddfed5c3520bb5177524
Sourabh Gaonkar <sourabh.gaonkar@intimetec.com>
2024-06-17 16:15:49 +0530

    updated code

--- Server/src/main/java/com/lnc/service/Authentication.java
+++ Server/src/main/java/com/lnc/service/Authentication.java
@@ -1,30 +1,38 @@
 package com.lnc.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lnc.DB.UserDetails;
 import com.lnc.model.Employee;
 import com.lnc.utils.FromJson;
 import com.lnc.utils.ToJson;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class Authentication {
   private final UserDetails user = new UserDetails();
+  private final Logger logger = Logger.getLogger(Authentication.class.getName());
 
   public Authentication() throws SQLException {}
 
-  public String authenticate(String jsonData) throws Exception {
-    FromJson converter = new FromJson();
-    String employeeID = converter.getJsonValue("employeeID", jsonData);
-    String password = converter.getJsonValue("password", jsonData);
+  public String authenticate(String jsonData) {
+    try {
+      FromJson converter = new FromJson();
+      String employeeID = converter.getJsonValue("employeeID", jsonData);
+      String password = converter.getJsonValue("password", jsonData);
 
-    if (user.validateEmployeeID(employeeID)) {
-      Employee employee = user.authenticateUser(employeeID, password);
-      if (employee != null) {
-        ToJson json = new ToJson();
-        return json.codeUserDetails(employee);
-      } else {
-        return "Wrong username, password.";
+      if (user.validateEmployeeID(employeeID)) {
+        Employee employee = user.authenticateUser(employeeID, password);
+        if (employee != null) {
+          ToJson json = new ToJson();
+          return json.codeUserDetails(employee);
+        } else {
+          return "Wrong username, password.";
+        }
       }
+      return "EmployeeID does not exist.";
+    } catch (Exception e) {
+      logger.severe(e.getMessage());
+      return "Wrong request format.";
     }
-    return "EmployeeID does not exist.";
   }
 }
